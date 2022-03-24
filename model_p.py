b44be49222a0e553b68d1eded6bed8750ddad8b8
PN1896 <p.noesel@gmx.de>
2022-03-24 12:25:31 +0100

    commit-neu

--- model_p.py
+++ model_p.py
@@ -1,11 +1,9 @@
-import numpy as np
 import pandas as pd
 from sklearn import model_selection
 from sklearn.linear_model import LinearRegression
 import pickle
 
 data = pd.read_csv("C:/Git-Test/SEII-1/data/auto-mpg.csv",";")
-
 
 #print(data.head())
 array = data.values
@@ -16,18 +14,14 @@ X = array[:,1:5]
 #print(X)
 print(len(Y))
 print(len(X))
-
-
-
 test_size = 0.33
 X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, Y, test_size=test_size,)
+
 model = LinearRegression()
-model =model.fit(X_train,Y_train)
+# load the model from disk
 
-y_pred = model.predict(X_test)
-#print(y_pred)
-# save the model to disk
 filename = 'finalized_model.sav'
-pickle.dump(model, open(filename, 'wb'))
-print(model.score(X_test, Y_test))
+loaded_model = pickle.load(open(filename, 'rb'))
+result = loaded_model.score(X_test, Y_test)
 
+print (result)
